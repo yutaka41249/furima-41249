df376ea669e109d1cd014402146d6bc11d90a1d3
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-17 15:43:52 +0900

    カラム追加
    usersテーブルにnicknameカラムをstring型で追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_17_030916) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_17_064104) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_17_030916) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
