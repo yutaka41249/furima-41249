700cd01ddb16102486015b5a650523e47ec5c847
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-19 20:43:54 +0900

    9行10行削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
-    devise_parameter_sanitizer.permit(:account_update,
-                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 end
