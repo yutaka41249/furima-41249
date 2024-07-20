702c8446d795fe0bc5b44e77c5608e39e2424722
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-20 19:49:40 +0900

    spec/factories/users.rb
    FactoryBotのダミーデータには、usersテーブルのカラムを全て用意。
    →nicknameからbirth_dateまで、全てのカラムを適切に用意する必要。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.last_name }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    nickname { Faker::Name.unique.name }
+    email { Faker::Internet.unique.email }
+    password { 'password123' }
     password_confirmation { password }
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    last_name_kana { 'タナカ' }
+    first_name_kana { 'タロウ' }
+    birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
