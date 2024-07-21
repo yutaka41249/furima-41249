33c677e5023234ff2fec8e613a7a5b0ecdca53d0
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-21 16:40:03 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_20_114614) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_21_073913) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_20_114614) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "products", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.integer "shipping_fee_id"
+    t.integer "prefecture_id"
+    t.integer "shipping_days_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
