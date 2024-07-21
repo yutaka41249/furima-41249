ebc79488a62eccf6e0f831a415db2f2b97118094
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-21 16:56:40 +0900

    バリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1 }
+  validates :image, presence: true
 end
