3a931b374717cbcaa3efa6b705a566387e96c857
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-21 17:01:02 +0900

    コード追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'products/new'
+  get 'products/create'
   devise_for :users
   root to:"items#index"
+  resources :products, only: [:new, :create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
