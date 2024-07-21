b34b680e2d3d7fec6cf6d219aa7e3c7d40320f29
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-21 17:27:17 +0900

    ファイル、フォルダ作成

---
+++ app/javascript/packs/calculate_profit.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const priceInput = document.getElementById('product_price');
+  const profitOutput = document.getElementById('profit');
+  const feeOutput = document.getElementById('fee');
+
+  priceInput.addEventListener('input', () => {
+    const price = priceInput.value;
+    const fee = Math.floor(price * 0.1);
+    const profit = price - fee;
+
+    feeOutput.textContent = `販売手数料: ${fee}円`;
+    profitOutput.textContent = `販売利益: ${profit}円`;
+  });
+});
