080e50cf65ac1c2dc44d667ec8f5757b66a85e10
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-22 21:58:56 +0900

    コード修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,12 +2,11 @@ document.addEventListener("turbo:load", function(){
   console.log("OK");  // JavaScriptが正しくロードされたことを確認
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-
     const inputValue = parseInt(priceInput.value); // 数値に変換
     const addTaxPrice = Math.floor(inputValue * 0.1); // 手数料計算
     const profit = inputValue - addTaxPrice; // 利益計算
-    document.getElementById("add-tax-price").textContent = addTaxPrice; // 手数料表示
-    document.getElementById("profit").textContent = profit; // 利益表示
+    document.getElementById("add-tax-price").textContent = addTaxPrice.toLocaleString(); // 手数料表示
+    document.getElementById("profit").textContent = profit.toLocaleString(); // 利益表示
 
   });
 });
