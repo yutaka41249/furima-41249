fd26aa6f1145a7ea833b91267b3dfd48af725593
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-22 12:03:26 +0900

    コード追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  validates :category_id, presence: true
 end
