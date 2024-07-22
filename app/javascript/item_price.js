534a67800689ba2d2f79d16226d1d81021ae2a3a
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-22 16:05:06 +0900

    コード追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("turbo:load", function(){
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    console.log(inputValue); // デバッグ用: 入力された値がコンソールに表示される
+    const inputValue = parseInt(priceInput.value); // 数値に変換
+    const addTaxPrice = Math.floor(inputValue * 0.1); // 手数料計算
+    const profit = inputValue - addTaxPrice; // 利益計算
+    document.getElementById("add-tax-price").textContent = addTaxPrice; // 手数料表示
+    document.getElementById("profit").textContent = profit; // 利益表示
   });
 });
