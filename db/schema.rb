2174c25598179fbeb1fa139368c1f900d243ecf9
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-22 19:24:41 +0900

    DB設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_22_081749) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_22_102352) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_22_081749) do
     t.integer "prefecture_id"
     t.integer "scheduled_delivery_id"
     t.integer "price"
+    t.string "image"
   end
 
   create_table "prefectures", charset: "utf8", force: :cascade do |t|
