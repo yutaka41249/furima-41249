db84620b306ecc96ff96352091c1e1c57b9d7cc6
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-22 12:12:29 +0900

    内容修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: 'メンズ' }
+    { id: 2, name: 'メンズ' },
     { id: 3, name: 'レディース' },
     { id: 4, name: 'ベビー・キッズ' },
     { id: 5, name: 'インテリア・住まい・小物' },
