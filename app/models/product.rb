ef6c9f53cea250e488315be2fb2574ba2c10ea2f
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 19:12:44 +0900

    コード修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
+  has_one_attached :image # 画像添付機能を追加
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1 }
-  validates :image, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_days_id, presence: true
 end
