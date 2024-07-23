ede615ea9befa86ea90c1f76ae6b53b780f9c033
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 19:26:19 +0900

    コード追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe Product, type: :model do
     @product = FactoryBot.build(:product)
   end
 
-  it '商品名が必須であること' do
-    @product.name = ''
-    @product.valid?
-    expect(@product.errors.full_messages).to include("Name can't be blank")
-  end
+  context 'バリデーションのテスト' do
+    it '商品名が必須であること' do
+      @product.name = ''
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
 
-  it '価格が必須であること' do
-    @product.price = ''
-    @product.valid?
-    expect(@product.errors.full_messages).to include("Price can't be blank")
+    it '価格が必須であること' do
+      @product.price = nil
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
   end
-
-  # 他のテストも同様に追加します
 end
