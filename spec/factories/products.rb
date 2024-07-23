840ed03076ad52eaf9478376a664ec6b83af6537
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 19:16:10 +0900

    コード追加

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -8,5 +8,10 @@ FactoryBot.define do
     shipping_fee_id { 1 }
     prefecture_id { 1 }
     shipping_days_id { 1 }
+
+    after(:build) do |product|
+      product.image.attach(io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png')), filename: 'test_image.png', content_type: 'image/png')
+
+
   end
 end
