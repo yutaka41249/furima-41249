760889060bda703ebe4da22a08bdedb28efa08d8
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 14:38:46 +0900

    コード変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-console.log("OK");  // JavaScriptが正しくロードされたことを確認
+
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./item_price"
