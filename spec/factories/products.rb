bbf36943b3a2a9b1aaf18999fc13f21184cff5c2
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 18:24:56 +0900

    コード変更

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     price { 1000 }
     category_id { 1 }
     condition_id { 1 }
-    shipping_fee_status_id { 1 }
+    shipping_fee_id { 1 }
     prefecture_id { 1 }
-    scheduled_delivery_id { 1 }
+    shipping_days_id { 1 }
   end
 end
