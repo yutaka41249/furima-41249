b5cec7c4e01e85e2ab595a203aa4af54f0bf87b1
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 21:21:23 +0900

    コード修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,25 +15,25 @@ RSpec.describe Product, type: :model do
     it '価格が必須であること' do
       @product.price = nil
       expect(@product).not_to be_valid
-      expect(@product.errors.full_messages).to include('Price is not a number')
+      expect(@product.errors.full_messages).to include("Price is not a number") # rubocop:disable Style/StringLiterals
     end
 
     it '価格が数値であること' do
       @product.price = 'abc'
       expect(@product).not_to be_valid
-      expect(@product.errors.full_messages).to include('Price is not a number')
+      expect(@product.errors.full_messages).to include("Price is not a number") # rubocop:disable Style/StringLiterals
     end
 
     it '価格が300以上であること' do
       @product.price = 299
       expect(@product).not_to be_valid
-      expect(@product.errors.full_messages).to include('Price must be greater than or equal to 300')
+      expect(@product.errors.full_messages).to include("Price must be greater than or equal to 300") # rubocop:disable Style/StringLiterals
     end
 
     it '価格が9999999以下であること' do
       @product.price = 10_000_000
       expect(@product).not_to be_valid
-      expect(@product.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      expect(@product.errors.full_messages).to include("Price must be less than or equal to 9999999") # rubocop:disable Style/StringLiterals
     end
   end
 end
