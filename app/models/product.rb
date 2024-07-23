d640c8079a88a34c8deb4045eb4ddeee03adde77
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-23 21:04:35 +0900

    コード修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,8 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  alidates :price, presence: true,
+                   numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, presence: true
   validates :condition_id, presence: true
   validates :shipping_fee_id, presence: true
