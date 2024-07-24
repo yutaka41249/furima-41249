03bf84f3089a2d32e20b4939615d11c73ab45740
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-24 17:05:03 +0900

    usersモデルの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
 
+  belongs_to :user
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee_status
