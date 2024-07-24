e35f2b19217a44f1aa37b3afa77ecc8b0b89ec53
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-24 14:50:51 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_status_id,
-                                 :prefecture_id, :scheduled_delivery_id, :price)
+                                 :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
