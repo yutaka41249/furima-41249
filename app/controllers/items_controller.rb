249831a2127ed0444c7d22d006d4c7f33b49eba8
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-25 18:15:19 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
   def index
     @items = Item.includes(:user).order(created_at: :desc) # 商品情報を全て取得し、作成日時の新しい順に並び替える
   end
@@ -13,12 +14,29 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    return unless current_user.id != @item.user.id
+
+    redirect_to root_path
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報が更新されました'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_status_id,
