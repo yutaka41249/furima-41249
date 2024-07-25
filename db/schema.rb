d5d40a8ddb9bd0476a5f93c375ffc08dfcfe59b3
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-26 01:01:31 +0900

    マイグレーションファイルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_25_033533) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_25_155856) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_25_033533) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "sold_out", default: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
