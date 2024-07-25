eda859e72ce040473eee1e9dcdf69ef15a418d14
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-25 19:55:28 +0900

    コード修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,20 +2,20 @@ const price = () => {
 
   const priceInput = document.getElementById("item-price");
 
-  priceInput.addEventListener("input", () => {
-    const inputValue = parseInt(priceInput.value); // 数値に変換
-    if (isNaN(inputValue) || inputValue < 0) { // 入力値が数値でない、または負の数値の場合
-
-      document.getElementById("add-tax-price").textContent = "—";
-      document.getElementById("profit").textContent = "—";
-      return;
-    }
-    const addTaxPrice = Math.floor(inputValue * 0.1); // 手数料計算
-    const profit = Math.floor(inputValue - addTaxPrice); // 利益計算
-    document.getElementById("add-tax-price").textContent = addTaxPrice.toLocaleString(); // 手数料表示
-    document.getElementById("profit").textContent = profit.toLocaleString(); // 利益表示
-
-  });
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = parseInt(priceInput.value); // 数値に変換
+      if (isNaN(inputValue) || inputValue < 0) { // 入力値が数値でない、または負の数値の場合
+        document.getElementById("add-tax-price").textContent = "—";
+        document.getElementById("profit").textContent = "—";
+        return;
+      }
+      const addTaxPrice = Math.floor(inputValue * 0.1); // 手数料計算
+      const profit = Math.floor(inputValue - addTaxPrice); // 利益計算
+      document.getElementById("add-tax-price").textContent = addTaxPrice.toLocaleString(); // 手数料表示
+      document.getElementById("profit").textContent = profit.toLocaleString(); // 利益表示
+    });
+  }
 };
 
   window.addEventListener("turbo:load", price);
