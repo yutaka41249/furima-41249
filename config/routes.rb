48356ef1292642d980cf6f8a8a49b4bec60a7c1e
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-26 18:05:12 +0900

    :edit, :update, :destroy削除
    :edit, :update, :destroyの指定は一旦消す。次回以降のブランチで追加。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show,:edit, :update, :destroy]
+  resources :items, only: [:index, :new, :create, :show]
   resources :orders, only: [:new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
