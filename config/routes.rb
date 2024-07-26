2521e49e56c2384d9985757d759a4e01bf5e1d82
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-26 16:00:36 +0900

    コード追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show,:edit, :update, :destroy]
+  resources :orders, only: [:new, :create]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
