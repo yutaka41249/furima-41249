b8a7c7a4a51763fc94b975f83a6bbdb3ff220476
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-29 17:01:25 +0900

    コメント修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,16 +11,16 @@ class ItemsController < ApplicationController
   end
 
   def new
-  #   @item = Item.new
+    @item = Item.new
   end
 
   def create
-  #   @item = Item.new(item_params)
-  #   if @item.save
-  #     redirect_to root_path
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
@@ -61,6 +61,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_status_id,
                                   :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-
-  end
+ end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,24 +1,7 @@
 class PurchasesController < ApplicationController
-  before_action :set_item
-  def new
-  end
+  # def new
+  # end
 
-  def create
-    if @item.update(purchase_params)
-      redirect_to item_path(@item), notice: '購入が完了しました'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def set_item
-    @item = Item.find(params[:item_id])
-  end
-
-  def purchase_params
-    # 購入に必要なパラメータを許可します
-    params.require(:purchase).permit(:user_id, :item_id)
-  end
+  # def create
+  # end
 end
