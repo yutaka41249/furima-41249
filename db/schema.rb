959fe68b58671d941e65800191fb9c710f95ae4d
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-29 21:51:29 +0900

    Orderモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_25_155856) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_29_124836) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_25_155856) do
     t.datetime "updated_at", null: false
     t.boolean "sold_out", default: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "purchases", charset: "utf8", force: :cascade do |t|
