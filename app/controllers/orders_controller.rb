a17911beb860f707c61f9bd25bededdf6840f329
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-30 15:40:08 +0900

    Ordersコントローラーを作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+end
