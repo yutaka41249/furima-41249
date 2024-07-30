45a4049dd088e793ddfacf9e8eed07a717fffede
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-30 18:30:22 +0900

    app/javascript/card.jsを作成
    app/javascript/card.jsを作成し、トークン化の処理を記述

---
+++ app/javascript/card.js
@@ -0,0 +1,28 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_***********************'); // PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById('charge-form');
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        const renderDom = document.getElementById('charge-form');
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        form.submit();
+      }
+    });
+    e.preventDefault();
+  });
+};
+
+window.addEventListener("turbo:load", pay);
