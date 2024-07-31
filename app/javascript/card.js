8f2ea8ea97e511c13e64dcfccfb2d429b81ddd3d
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-31 16:29:38 +0900

    コード編集

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
-  const publicKey = gon.public_key;
-  const payjp = Payjp(publicKey);
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -12,21 +12,22 @@ const pay = () => {
 
   const form = document.getElementById('charge-form');
   form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) {
+    e.preventDefault(); // ここでデフォルトのフォーム送信を防止
+    payjp.createToken(numberElement).then((response) => {
       if (response.error) {
+        // エラー処理
+        console.error(response.error.message);
       } else {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
+        numberElement.clear();
+        expiryElement.clear();
+        cvcElement.clear();
+        document.getElementById("charge-form").submit();
       }
-      numberElement.clear();
-      expiryElement.clear();
-      cvcElement.clear();
-      document.getElementById("charge-form").submit();
     });
-    e.preventDefault();
   });
 };
 
