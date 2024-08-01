80ba3db693d4f5300d257b35eca7c6eb6cabb776
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-01 20:20:38 +0900

    コード修正
    ApplicationControllerにgonの設定を追加します。これにより、どのコントローラーでもgonを利用できるようになります。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_gon
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
+
+  def set_gon
   end
 end
