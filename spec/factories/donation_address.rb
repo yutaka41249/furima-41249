64ff52f6985ce03661faa696d8862f6f7647e24f
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-01 09:38:03 +0900

    コード修正

--- spec/factories/donation_address.rb
+++ spec/factories/donation_address.rb
@@ -1,10 +1,9 @@
-# spec/factories/donation_addresses.rb
 FactoryBot.define do
   factory :donation_address do
     postal_code { '123-4567' }
-    prefecture_id { 1 }
+    prefecture_id { 2 }
     city { '東京都' }
-    address { '1-1' }
+    house_number { '1-1' }
     building_name { '東京ハイツ' }
     phone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
