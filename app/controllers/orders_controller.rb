97c6368479ed934e58e858b436c374751e4d05d0
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-01 18:20:49 +0900

    コード修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
-  before_action :set_item, only: [:index, :create]
+  before_action :set_item, only: [:index, :new, :create]
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
