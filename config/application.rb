b95a214b656b3f4427456a0e9a65c39e1a17f22c
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-02 12:12:04 +0900

    config.gon.global = false追加

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Furima
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.gon.global = false
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
