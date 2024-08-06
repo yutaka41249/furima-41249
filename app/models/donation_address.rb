0594aca4a751b3340357f6ce5c65513cea3ed8ab
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-06 14:47:59 +0900

    コード修正

--- app/models/donation_address.rb
+++ app/models/donation_address.rb
@@ -1,12 +1,12 @@
 class DonationAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
-    validates :address
+    validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Must be 10 or 11 digits' }
     validates :user_id
     validates :item_id
