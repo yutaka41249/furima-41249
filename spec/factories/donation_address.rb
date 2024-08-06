100553bdb26edc270170a11918cf60f56a2db863
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-06 11:40:16 +0900

    コード修正
    userとitemのアソシエーションは不要

--- spec/factories/donation_address.rb
+++ spec/factories/donation_address.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     building_name { '東京ハイツ' }
     phone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
-    association :user
-    association :item
   end
 end
