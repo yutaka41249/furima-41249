99e6d6ba5bd13620a1f73db11e815bbff1fa479f
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-06 15:15:09 +0900

    コード修正
    現状だと、出品者と購入者が同じユーザーになってしまうので、@itemは下記のように記述し直しをする

--- spec/models/donation_address_spec.rb
+++ spec/models/donation_address_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe DonationAddress, type: :model do
   before do
     @user = FactoryBot.create(:user)
-    @item = FactoryBot.create(:item, user: @user)
+    @item = FactoryBot.create(:item)
     @donation_address = FactoryBot.build(:donation_address, user_id: @user.id, item_id: @item.id)
   end
 
