d90ef3b9e3764c4cc3463704d1e9eb03d510bdd6
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-06 18:42:14 +0900

    コード修正

--- app/models/donation_address.rb
+++ app/models/donation_address.rb
@@ -1,6 +1,7 @@
 class DonationAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :address, :building_name, :phone_number, :user_id, :item_id,
+                :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
