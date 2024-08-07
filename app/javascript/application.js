ea89ad5fd690091e243287d733e0ddba852de25d
yutaka41249 <yutaka.oda0821@gmail.com>
2024-08-07 12:57:09 +0900

    コード修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,5 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "card"
+import "item_price"
 
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -19,3 +19,6 @@ const price = () => {
 };
 
   window.addEventListener("turbo:load", price);
+  window.addEventListener("turbo: render", price);
+
+
